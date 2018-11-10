b61391563fcdf713aa1871afdd901358286be701
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-10 18:36:15 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 hg clone http://hg.openjdk.java.net/jdk8/jdk8/langtools langtools
 cd langtools
 
-for i in $(grep -r -l 'System.exit\(' src/share/classes); do
+for i in $(grep -r -l 'System.exit(' src/share/classes); do
   sed -i -e 's/System.exit(/throw new RuntimeException("Exit Code: " + /g' $i
   echo "Patched: $i"
 done
