e3256daa642e64e0fa1ff1c042e5fd4945e4274d
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-10 18:21:34 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ set -e
 hg clone http://hg.openjdk.java.net/jdk8/jdk8/langtools langtools
 cd langtools
 
+for i in $(grep -r -l "System.exit" src/share/classes); do
+  sed -i -e 's/System.exit/throw new java.lang.RuntimeException/g' $i
+  echo "Patched: $i"
+done
+
 ant -buildfile make/build.xml -Dboot.java.home=${JAVA_HOME}
