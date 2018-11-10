70ac0c9d3bf9fdfe8c62410337d940759f4704d1
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-10 14:53:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-hg clone http://hg.openjdk.java.net/jdk8/jdk8/langtools langtools
+hg clone http://hg.openjdk.java.net/jdk7/jdk7/langtools langtools
 cd langtools
 
-ant -buildfile make/build.xml
+ant -buildfile make/build.xml -Dboot.java.home=${JAVA_HOME}
