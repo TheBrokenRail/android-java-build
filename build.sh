fe1ccd6f82ab663393d9a2abac68067942ed08f3
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-13 19:20:09 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -e
 hg clone http://hg.openjdk.java.net/jdk8/jdk8/langtools langtools
 cd langtools
 
-for i in $(grep -r -l 'System.exit' src/share/classes); do
-  sed -i -e 's/System.exit/if (true) throw new SecurityException/g' $i
+for i in $(grep -r -l 'System.exit(' src/share/classes); do
+  sed -i -e 's/System.exit(/if (true) throw new SecurityException("Exit Code: " + /g' $i
   echo "Patched: $i"
 done
 
@@ -20,8 +20,8 @@ cd dalvik/dx
 shopt -s globstar
 
 
-for i in $(grep -r -l 'System.exit' src/share/classes); do
-  sed -i -e 's/System.exit/if (true) throw new SecurityException/g' $i
+for i in $(grep -r -l 'System.exit(' src/share/classes); do
+  sed -i -e 's/System.exit(/if (true) throw new SecurityException("Exit Code: " + /g' $i
   echo "Patched: $i"
 done
 
