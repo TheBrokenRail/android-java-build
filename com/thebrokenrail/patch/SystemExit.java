ed0028333fc73d86b2e8e633b7d75a4e04080764
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-13 19:05:38 -0500

    Delete SystemExit.java

--- com/thebrokenrail/patch/SystemExit.java
+++
@@ -1,7 +0,0 @@
-package com.thebrokenrail.patch;
-
-public class SystemExit extends RuntimeException {
-  public static void exit(int signal) {
-    throw new SystemExit(String.valueOf(signal));
-  }
-}
